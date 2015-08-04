7216d3f693df906dd5584e29381619fb32ff2c8f
Lauren <lauren.eastridge@formidablelabs.com>
2015-08-04 10:43:14 -0700

    non working, progress towards axis paths

--- src/components/victory-axis.jsx
+++ src/components/victory-axis.jsx
@@ -21,6 +21,9 @@ class VictoryAxis extends React.Component {
       min: style.margin,
       max: style.width - style.margin
     };
+
+    this.state.x.ticks = this.getTicks("x");
+    this.state.y.ticks = this.getTicks("y");
   }
 
   getStyles() {
@@ -43,49 +46,62 @@ class VictoryAxis extends React.Component {
     }, this.props.style);
   }
 
-  getXTransform() {
-    const range = this.state.y.range;
-    return "translate(" + 0 + "," + range.max + ")";
+  getTransform(type) {
+    const range = this.state[type].range;
+    return type === "x" ? "translate(" + 0 + "," + range.max + ")"
+      : "translate(" + range.min + "," + 0 + ")";
   }
 
-  getYTransform() {
-    const range = this.state.x.range;
-    return "translate(" + range.min + "," + 0 + ")";
+  getScale(type) {
+    const scale = type === "x" ?
+      this.props.x.scale().range([this.state.x.range.min, this.state.y.range.max])
+      : this.props.y.scale().range([this.state.x.range.max, this.state.y.range.min]);
+    return scale.domain(this.props[type].domain.min, this.props[type].domain.max)
   }
 
-  getXScale() {
-    const scale = this.props.x.scale().range([this.state.x.range.min, this.state.y.range.max]);
-    return scale.domain([this.props.x.domain.min, this.props.x.domain.max]);
+  getTicks(type) {
+    if (this.props[type].tickValues) {
+      return this.props[type].tickValues
+    }
+    return this.props[type].scale(this.props[type]).ticks(this.props[type].ticks)
   }
 
-  getYScale() {
-    const scale = this.props.y.scale().range([this.state.x.range.max, this.state.y.range.min]);
-    return scale.domain([this.props.y.domain.min, this.props.y.domain.max]);
+  getTickSpacing(type) {
+    const tickSize = this.props[type].tickSize;
+    return Math.max(tickSize.innerTickSize, 0) + tickSize.tickPadding
+  }
+
+  getAxisPath(type) {
+    const orientation = this.props[type].orient
+    if (orientation === "top" || orientation === "bottom") {
+
+    }
   }
 
   componentDidMount() {
-    const xAxisFunction = d3.svg.axis()
-      .scale(this.getXScale())
-      .orient(this.props.x.orient)
-      .ticks(5, 10)
-      .tickValues(this.props.x.tickValues);
-    const yAxisFunction = d3.svg.axis()
-      .scale(this.getYScale())
-      .orient(this.props.y.orient)
-      .ticks(5, 10)
-      .tickValues(this.props.y.tickValues);
+    console.log();
+    // const xAxisFunction = d3.svg.axis()
+    //   .scale(this.getXScale())
+    //   .orient(this.props.x.orient)
+    //   .ticks(5, 10)
+    //   .tickValues(this.props.x.tickValues);
+    // const yAxisFunction = d3.svg.axis()
+    //   .scale(this.getYScale())
+    //   .orient(this.props.y.orient)
+    //   .ticks(5, 10)
+    //   .tickValues(this.props.y.tickValues);
 
-    const xAxis = xAxisFunction(d3.select(React.findDOMNode(this.refs.xAxis)));
-    const yAxis = yAxisFunction(d3.select(React.findDOMNode(this.refs.yAxis)));
-    this.setState({xAxis, yAxis});
+    // const xAxis = xAxisFunction(d3.select(React.findDOMNode(this.refs.xAxis)));
+    // const yAxis = yAxisFunction(d3.select(React.findDOMNode(this.refs.yAxis)));
+    // this.setState({xAxis, yAxis});
   }
 
   render() {
     const styles = this.getStyles();
     return (
       <g>
-        <g ref="xAxis" style={styles.xAxis} transform={this.getXTransform()}>{this.state.xAxis}</g>
-        <g ref="yAxis" style={styles.yAxis} transform={this.getYTransform()}>{this.state.yAxis}</g>
+        <g ref="xAxis" style={styles.xAxis} transform={this.getTransform("x")}></g>
+        <g ref="yAxis" style={styles.yAxis} transform={this.getTransform("y")}></g>
       </g>
     );
   }
@@ -104,11 +120,14 @@ VictoryAxis.propTypes = {
       max: React.PropTypes.number
     }),
     scale: React.PropTypes.func, // is this true, or can we pass a string?
-    ticks: React.PropTypes.oneOfType([
-      React.PropTypes.array,
-      React.PropTypes.number
-    ]),
-    tickValues: React.PropTypes.array
+    ticks: React.PropTypes.number,
+    tickValues: React.PropTypes.array,
+    tickSize: React.PropTypes.shape({
+      innerTickSize: React.PropTypes.number,
+      outerTickSize: React.PropTypes.number,
+      tickPadding: React.PropTypes.number
+    }),
+    tickFormat: React.PropTypes.func
   }),
   y: React.PropTypes.shape({
     domain: React.PropTypes.shape({
@@ -121,11 +140,14 @@ VictoryAxis.propTypes = {
       max: React.PropTypes.number
     }),
     scale: React.PropTypes.func, // is this true, or can we pass a string?
-    ticks: React.PropTypes.oneOfType([
-      React.PropTypes.array,
-      React.PropTypes.number
-    ]),
-    tickValues: React.PropTypes.array
+    ticks: React.PropTypes.number,
+    tickValues: React.PropTypes.array,
+    tickSize: React.PropTypes.shape({
+      innerTickSize: React.PropTypes.number,
+      outerTickSize: React.PropTypes.number,
+      tickPadding: React.PropTypes.number
+    }),
+    tickFormat: React.PropTypes.func
   })
 };
 
